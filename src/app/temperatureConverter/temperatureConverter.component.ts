09b87752537c23a8c868b9bfdc6ef787d823eb5c
User <user.push@hackerrank.workspace>
2021-04-19 08:45:17 +0000

    [Admin Commit] save changes
    Commit by HackerRank

--- src/app/temperatureConverter/temperatureConverter.component.ts
+++ src/app/temperatureConverter/temperatureConverter.component.ts
@@ -8,9 +8,21 @@ import {Component, Input, OnInit} from '@angular/core';
 
 export class TemperatureConverter implements OnInit {
 
+  C = 0;
+  F = 0;
+
   ngOnInit() {
-    // C = (F − 32) × 5/9
-    // F = C*9/5 + 32
+
+    // this.C = this.F − 32;
+    // this.C = this.C  × 5 / 9;
+    // this.F = this.C*9/5 + 32;
+
+      // this.C = parseInt(this.C);
+
+      this.C = this.F * 5 / 9 + 32;
+      this.F = this.C * 9 / 5 + 32;
+
+
   }
 
 }
